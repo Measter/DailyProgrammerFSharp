99fd33820bcf68afae0b801f39631a3d19552095
Stuart Haidon <shaidon@serayen.com>
2015-08-17 12:49:35 +0100

    #228 Easy: Added detection of reverse order

--- DPC_228/Easy.fsx
+++ DPC_228/Easy.fsx
@@ -8,14 +8,19 @@ System.Console.SetOut(OutStream) |> ignore
 let sw = System.Diagnostics.Stopwatch.StartNew()
 let lines = List.ofSeq (System.IO.File.ReadAllLines (Difficulty+"Data/Input.txt")) 
 
+type Order = Ordered | NotOrdered | Reverse
 
 let InOrder = "IN ORDER"
 let NotInOrder = "NOT IN ORDER"
+let InReverseOrder = "REVERSE ORDER"
 
 type Word = { Word:string; CharCodes:int list }
 
 let ParseStrings s = { Word = s; CharCodes = [for ch in s do yield int (System.Char.ToLower ch) ] }
-let IsOrdered l = l = (l |> List.sort)
+let IsOrdered l = match l with  
+                    | _ when l = (l |> List.sort) -> Ordered
+                    | _ when l = (l |> List.sort |> List.rev ) -> Reverse
+                    | _ -> NotOrdered 
 
 match lines with
 | [] -> printfn "Error: Input List is empty."
@@ -23,8 +28,9 @@ match lines with
     let parsed = tail |> List.map ParseStrings
     for w in parsed do
         match IsOrdered w.CharCodes with
-            | true -> printfn "%s %s" w.Word InOrder
-            | false -> printfn "%s %s" w.Word NotInOrder
+            | Ordered -> printfn "%s %s" w.Word InOrder
+            | NotOrdered -> printfn "%s %s" w.Word NotInOrder
+            | Reverse -> printfn "%s %s" w.Word InReverseOrder
     
 sw.Stop()
 printfn "Time: %A" sw.Elapsed
