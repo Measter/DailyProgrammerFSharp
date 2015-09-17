f56080286f40a59a14e8054305b0155dc82fbcb0
Stuart Haidon <shaidon@serayen.com>
2015-09-17 12:16:24 +0100

    DPC232: Now uses Divide-and-Conquer  algorithm

--- DPC_232/Medium.fsx
+++ DPC_232/Medium.fsx
@@ -1,6 +1,8 @@
 // Daily Programmer Challenge #232: Where Should Grandma's House Go
 // https://www.reddit.com/r/dailyprogrammer/comments/3l61vx
 
+open System.Threading.Tasks
+
 let Difficulty = "Medium"
 
 let OutStream = new System.IO.StreamWriter(Difficulty+"Data/Output.txt", false)
@@ -9,13 +11,14 @@ let sw = System.Diagnostics.Stopwatch.StartNew()
 let lines = List.ofSeq (System.IO.File.ReadAllLines (Difficulty+"Data/Input.txt")) 
 
 type Coordinate = {X:float; Y:float} 
+type CoordinatePair = {First:Coordinate; Second:Coordinate}
 
 let rec TryParseCoordinates (lines:string list) =
     match lines with
     | this::rest ->
-        let parts = this.[1 .. this.Length-2].Split([|", "|], System.StringSplitOptions.None)
-        let parsedX, x = System.Double.TryParse parts.[0]
-        let parsedY, y = System.Double.TryParse parts.[1]
+        let parts = this.[1 .. this.Length-2].Split([|","|], System.StringSplitOptions.None)
+        let parsedX, x = System.Double.TryParse (parts.[0].Trim())
+        let parsedY, y = System.Double.TryParse (parts.[1].Trim())
 
         match parsedX && parsedY, rest.Length with
         | true, 0 -> [Some {X = x; Y=y}]
@@ -24,7 +27,7 @@ let rec TryParseCoordinates (lines:string list) =
         | false, _ -> None :: TryParseCoordinates rest
     | [] -> []
 
-let GetRadius (x:Coordinate) (y:Coordinate) = System.Math.Sqrt(System.Math.Pow(x.X - y.X, 2.0) + System.Math.Pow(x.Y-y.Y, 2.0))
+let GetRadius (x:Coordinate) (y:Coordinate) = System.Math.Pow(x.X - y.X, 2.0) + System.Math.Pow(x.Y-y.Y, 2.0)
 
 // This function shamelessly ripped from http://stackoverflow.com/a/4495708/4194885
 let rec combinations acc size set = seq {
@@ -34,6 +37,33 @@ let rec combinations acc size set = seq {
         if n >= 0 then yield! combinations acc n xs
     | 0, [] -> yield acc
     | _, [] -> () }
+
+let rec GetMinDistance (list:Coordinate list) =
+    if list.Length > 650 then
+        let left, right = list.[0 .. list.Length/2], list.[list.Length/2 .. list.Length-1]
+
+        let rhsTask = Task.Factory.StartNew<CoordinatePair>( (fun () -> GetMinDistance right ) )
+        let lhsTask = Task.Factory.StartNew<CoordinatePair>( (fun () -> GetMinDistance left ) )
+    
+        let result = Task.WhenAll([rhsTask;lhsTask])
+        let minLeft, minRight = result.Result.[0], result.Result.[1]
+
+        let center = (list.[list.Length/2].X + list.[list.Length/2+1].X) / 2.0
+        let radLeft = System.Math.Sqrt (GetRadius minLeft.First minLeft.Second)
+        let radRight = System.Math.Sqrt (GetRadius minRight.First minRight.Second)
+        let minRad = System.Math.Min(radLeft, radRight)                       
+        
+        let centreColumn = list |> List.filter (fun c -> c.X > (center-minRad) && c.X < (center+minRad))
+
+        if centreColumn = list || centreColumn = [] then
+            if minRad = radLeft then minLeft
+            else minRight
+        else
+            GetMinDistance centreColumn
+    else
+        let combs = list |> combinations [] 2 |> Seq.minBy (fun l -> GetRadius l.Head l.Tail.Head)         
+        {First = combs.Head; Second = combs.Tail.Head}
+    
 
 match lines with
 | [] -> printfn "Error: Input List is empty."
@@ -46,13 +76,10 @@ match lines with
 
         if coords |> List.exists (fun x -> x.IsNone) then printfn "One or more inputs were not parsed."
         else
-            let coords = coords |> List.map (fun x -> x.Value)
-            let min = coords |> combinations [] 2 |> Seq.minBy ( fun l -> let x,y = l.Head, l.Tail.Head
-                                                                          GetRadius x y)
+            let coords = coords |> List.map (fun x -> x.Value) |> List.sortBy (fun x -> x.X)                 
+            let pair = GetMinDistance coords
 
-            let first, second = min.Head, min.Tail.Head
-
-            printfn "(%s,%s) (%s,%s)" (first.X.ToString()) (first.Y.ToString()) (second.X.ToString()) (second.Y.ToString())
+            printfn "(%s,%s) (%s,%s)" (pair.First.X.ToString()) (pair.First.Y.ToString()) (pair.Second.X.ToString()) (pair.Second.Y.ToString())
     | _ -> printfn "Error: Input data length mismatch."
     
 sw.Stop()
